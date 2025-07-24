9d9b0bdfaaa5966ec5a58bbce1f2036761482997
zshawar <zshawar@uncc.edu>
2025-07-24 18:16:59 -0400

    fixed relationship and added h2 database

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -48,6 +48,8 @@ public class BootstrapData implements CommandLineRunner {
 
         ericSaved.getBooks().add(dddSaved);
         rodSaved.getBooks().add(noEJBSaved);
+        dddSaved.getAuthors().add(ericSaved);
+        noEJBSaved.getAuthors().add(rodSaved);
 
         Publisher hc = new Publisher();
         hc.setPublisherName("HarperCollins");
